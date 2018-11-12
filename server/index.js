14fde1d84b2d57f289df1d804a55ab18d8a092d1
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-11-12 17:11:50 -0600

    oauth with google done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 /* eslint-disable prefer-destructuring */
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('access Token', accessToken);
+  console.log('refreshToken', refreshToken);
+  console.log('profile', profile);
+  console.log('done', done);
+}));
+
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.listen(PORT);
