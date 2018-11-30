48c64dda393d2780cb574d28a02130875bdba158
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-11-29 23:58:05 -0600

    materialize css lib

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -4,9 +4,15 @@ import React, { Component } from 'react'
 class Header extends Component{
     render(){
         return(
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="left brand-logo"> Email</a>
+                    <ul className="right hide-on-med-and-down">
+                         <li><a href="#">Sass</a></li>
+                         <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
