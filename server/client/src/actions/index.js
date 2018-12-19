534b1fc0b5b7a8f0d81365037cec5a4bd6bd493d
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-12-19 16:34:28 -0600

    eslint airbnb

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,5 +6,5 @@ export const fetchUser = () => async dispatch => {
 // place 'await'in front of the axios func
     const res = await axios.get('/api/current_user'); 
 
-    dispatch({ type: FETCH_USER, payload: res });
+    dispatch({ type: FETCH_USER, payload: res.data });
 }
